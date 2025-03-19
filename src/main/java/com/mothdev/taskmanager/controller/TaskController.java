b51d736b1e0714ff5fa93f2683ed04e4f9e9e555
Alarcon <franalarconrubio@icloud.com>
2025-03-19 19:47:49 +0100

    add swagger documentation

--- src/main/java/com/mothdev/taskmanager/controller/TaskController.java
+++ src/main/java/com/mothdev/taskmanager/controller/TaskController.java
@@ -3,6 +3,9 @@ package com.mothdev.taskmanager.controller;
 import com.mothdev.taskmanager.dto.TaskRequestDTO;
 import com.mothdev.taskmanager.dto.TaskResponseDTO;
 import com.mothdev.taskmanager.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +22,11 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(summary = "Obtener una tarea por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea encontrada"),
+            @ApiResponse(responseCode = "404", description = "Tarea no encontrada")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> getTaskById(@PathVariable Long id) {
         return this.taskService.getTaskById(id).
@@ -26,6 +34,11 @@ public class TaskController {
                 orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Obtener todas las tareas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de tareas obtenida con éxito"),
+            @ApiResponse(responseCode = "204", description = "No hay tareas disponibles")
+    })
     @GetMapping
     public ResponseEntity<List<TaskResponseDTO>> getAllTasks() {
         List<TaskResponseDTO> allTasks = taskService.getAllTasks();
@@ -37,16 +50,32 @@ public class TaskController {
         return ResponseEntity.ok(allTasks);
     }
 
+
+    @Operation(summary = "Crear una nueva tarea")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea creada con éxito"),
+            @ApiResponse(responseCode = "400", description = "Datos inválidos")
+    })
     @PostMapping
     public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskRequestDTO task) {
         return ResponseEntity.ok(taskService.createTask(task));
     }
 
+    @Operation(summary = "Actualizar una tarea existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarea actualizada con éxito"),
+            @ApiResponse(responseCode = "404", description = "Tarea no encontrada")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable Long id, @Valid @RequestBody TaskRequestDTO task) {
         return ResponseEntity.ok(taskService.updateTask(id, task));
     }
 
+    @Operation(summary = "Eliminar una tarea")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Tarea eliminada"),
+            @ApiResponse(responseCode = "404", description = "Tarea no encontrada")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
